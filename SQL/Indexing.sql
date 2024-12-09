91fe146988b44ac9a7c5542928febf50f0de5acf
RiceTooCold <josephcc05@gmail.com>
2024-12-09 15:10:13 +0800

    put index in backup

--- SQL/Indexing.sql
+++ SQL/Indexing.sql
@@ -1,9 +1,9 @@
 Create Index game_id
-On BETS_ODD_RECORD(Game_id)
+On BET_ODDS_RECORD(Game_id);
 
-Drop Index If Exists game_id
+Drop Index If Exists game_id;
 --------------------------------
 Create Index team_id
-On GAME(home_team_id, away_team_id)
+On GAME(home_team_id, away_team_id);
 
-Drop Index If Exists team_id
+Drop Index If Exists team_id;
